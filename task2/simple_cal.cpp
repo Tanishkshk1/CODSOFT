0351fcce632a7c5248b6e7427a8369b9da1a3ef9
Tanish <2821108.cse@pietgroup.co.in>
2023-09-11 21:19:59 +0530

    Done task2

---
+++ task2/simple_cal.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    char operation;
+    double num1, num2;
+
+    cout << "Simple Calculator\n";
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
+
+    cout << "Enter an operation (+, -, *, /): ";
+    cin >> operation;
+
+    switch (operation) {
+        case '+':
+            cout << "Result: " << num1 + num2 << endl;
+            break;
+        case '-':
+            cout << "Result: " << num1 - num2 << endl;
+            break;
+        case '*':
+            cout << "Result: " << num1 * num2 << endl;
+            break;
+        case '/':
+            if (num2 != 0) {
+                cout << "Result: " << num1 / num2 << endl;
+            } else {
+                cout << "Error: Division by zero is not allowed." << endl;
+            }
+            break;
+        default:
+            cout << "Error: Invalid operation." << endl;
+            break;
+    }
+
+    return 0;
+}
+
